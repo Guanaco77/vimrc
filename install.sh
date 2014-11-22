8836c4713ae53ae5d4dc3d64681c5671fdd7ea5e
Tsotne Shonia <tshonia@ulb.ac.be>
2014-11-22 15:33:28 +0100

    [script]now appends basic 256 solarized stuff

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo 'syntax enable' >> vimrc
+echo 'set t_Co=256' >> vimrc
+echo 'let g:solarized_termcolors=256' >> vimrc
+echo 'set background=dark' >> vimrc
+echo 'let g:solarized_visibility = "high"' >> vimrc
+echo 'let g:solarized_contrast = "high"' >> vimrc
+echo 'colorscheme solarized' >> vimrc
+
